64cc3a3e96622c890f1d37c0d644302548a72f60
Dylandy Chang <steven80919@gmail.com>
2014-05-23 21:45:54 +0800

    cleaner build stock

--- stock_rails/script/build_stock.rb
+++ stock_rails/script/build_stock.rb
@@ -26,20 +26,11 @@ module BuildStock
     stock_filter()
   end
   def self.stock_filter
-    target_1=[]
-    target_2=[]
-    data_1=[]
-    data_2=[]
-    stock =[]
+    target_1=[];target_2=[];data_1=[];data_2=[];stock =[]
 
     #create array for request 
-    (1101..5601).each do |t|
-      target_1 << t.to_s+".TW"
-    end
-
-    (5602..9962).each do |t|
-      target_2 << t.to_s+".TW"
-    end
+    (1101..5601).each do |t| target_1 << t.to_s+".TW";end
+    (5602..9962).each do |t| target_2 << t.to_s+".TW";end
 
     data_1 = YahooFinance.quotes(target_1,[:ask, :bid, :high, :low, :open, :close, :previous_close, :volume, :name],{raw:false})
     data_2 = YahooFinance.quotes(target_2,[:ask, :bid, :high, :low, :open, :close, :previous_close, :volume, :name],{raw:false})
@@ -56,18 +47,10 @@ module BuildStock
       end
     end
 
-    target_1.clear
-    target_2.clear
-    data_1.clear
-    data_2.clear
+    target_1.clear;target_2.clear;data_1.clear;data_2.clear
  
-    (1101..5601).each do |t|
-      target_1 << t.to_s+".TWO"
-    end
-
-    (5602..9962).each do |t|
-      target_2 << t.to_s+".TWO"
-    end
+    (1101..5601).each do |t| target_1 << t.to_s+".TWO";end
+    (5602..9962).each do |t| target_2 << t.to_s+".TWO";end
 
     data_1 = YahooFinance.quotes(target_1,[:ask, :bid, :high, :low, :open, :close, :previous_close, :volume, :name],{raw:false})
     data_2 = YahooFinance.quotes(target_2,[:ask, :bid, :high, :low, :open, :close, :previous_close, :volume, :name],{raw:false})
