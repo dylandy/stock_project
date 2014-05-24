7e4f23d37988750c908458bf9e7a082e7e76b99f
Dylandy Chang <steven80919@gmail.com>
2014-05-25 00:04:48 +0800

    shorten code

--- stock_rails/script/build_stock.rb
+++ stock_rails/script/build_stock.rb
@@ -27,37 +27,24 @@ module BuildStock
   end
   def self.stock_filter
     target_1=[];target_2=[];target_3=[];target_4=[];data_1=[];data_2=[];data_3=[];data_4=[];stock =[]
-
     #create array for request 
     (1101..5601).each do |t| target_1 << t.to_s+".TW";target_3 << t.to_s+".TWO";end
     (5602..9962).each do |t| target_2 << t.to_s+".TW";target_4 << t.to_s+".TWO";end
+    
     puts "fetching data"
+
     data_1 = YahooFinance.quotes(target_1,[:ask, :bid, :high, :low, :open, :close, :previous_close, :volume, :name],{raw:false})
     data_2 = YahooFinance.quotes(target_2,[:ask, :bid, :high, :low, :open, :close, :previous_close, :volume, :name],{raw:false})
     data_3 = YahooFinance.quotes(target_3,[:ask, :bid, :high, :low, :open, :close, :previous_close, :volume, :name],{raw:false})
     data_4 = YahooFinance.quotes(target_4,[:ask, :bid, :high, :low, :open, :close, :previous_close, :volume, :name],{raw:false})
+    
     puts "data fetched"
     
-    data_1.each do |t|
-      if t.ask != 0
-        stock << t
-      end
-    end
-    data_2.each do |t|
-      if t.ask != 0
-        stock << t
-      end
-    end
-    data_3.each do |t|
-      if t.ask != 0
-        stock << t
-      end
-    end
-    data_4.each do |t|
-      if t.ask != 0
-        stock << t
-      end
-    end
+    data_1.each do |t| if t.ask != 0 then stock << t end;end
+    data_2.each do |t| if t.ask != 0 then stock << t end;end
+    data_3.each do |t| if t.ask != 0 then stock << t end;end
+    data_4.each do |t| if t.ask != 0 then stock << t end;end
+    
     puts "stock saved"
 
   #目前 stock 獲得最新的所有資料，下面存入資料庫
