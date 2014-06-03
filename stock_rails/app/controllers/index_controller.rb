3532d694354828fb6f8894b420f1f6061b8e7189
Dylandy Chang <steven80919@gmail.com>
2014-06-03 17:42:08 +0800

    add most grow block to index page

--- stock_rails/app/controllers/index_controller.rb
+++ stock_rails/app/controllers/index_controller.rb
@@ -1,4 +1,22 @@
 class IndexController < ApplicationController
   def index
+    tmp = 0;tmp1 = 0
+    item = ItemTable.all
+
+    item.each do |i|
+      if tmp == 0
+        tmp = i.close / i.prev_close
+        tmp1 = i
+      elsif tmp < i.close / i.prev_close
+        tmp = i.close / i.prev_close
+        tmp1 = i
+      end
+    end
+
+    @most_grow_name = tmp1.name.gsub!(/[^0-9A-Za-z.\- ]/, '')
+    @most_grow_id = tmp1.item_id
+    @most_grow_percent = (tmp*100).round.to_f / 100
+    @most_grow_open = tmp1.open
+    @most_grow_close = tmp1.close
   end
 end
