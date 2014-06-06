b418843b9371deca4b1b2408c12a80966a5225e9
Dylandy Chang <steven80919@gmail.com>
2014-06-06 21:09:47 +0800

    improve user checking with no account exist

--- stock_rails/app/controllers/sign_controller.rb
+++ stock_rails/app/controllers/sign_controller.rb
@@ -2,21 +2,23 @@ class SignController < ApplicationController
   def index
   end
   def check
-    if User.where(:account => params[:account]) && User.where(:account => params[:account]).first.status != 1
-      check = params[:pwd]
-      check = Digest::SHA1.hexdigest check
-      if User.where(:account => params[:account]).first.pwd == check
-        checker = User.where(:account => params[:account]).first
-        checker.status = 1
-        checker.save
-        @checking = 1
+    if !params[:account].empty?
+      if !User.where(:account => params[:account]).empty? && User.where(:account => params[:account]).first.status != 1
+        check = params[:pwd]
+        check = Digest::SHA1.hexdigest check
+        if User.where(:account => params[:account]).first.pwd == check
+          checker = User.where(:account => params[:account]).first
+          checker.status = 1
+          checker.save
+          @checking = 1
+        else
+          @checking = 0
+        end
+      elsif User.where(:account => params[:account]).empty?
+        @checking = 2
       else
-        @checking = 0
+        @checking = 3
       end
-    elsif User.where(:account => params[:account]).empty?
-      @checking = 2
-    else
-      @checking = 3
     end
   end
 end
