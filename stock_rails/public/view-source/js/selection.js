a91a615fe553dd46b01e074906b6be5aae1920ea
Dylandy Chang <steven80919@gmail.com>
2014-06-10 16:35:32 +0800

    click template and then searching box appear

--- stock_rails/public/view-source/js/selection.js
+++ stock_rails/public/view-source/js/selection.js
@@ -1,9 +1,5 @@
-var addSelection = function(){
-  var clone = $('.template').clone();
-  clone.removeClass('template');
-  clone.find('.add-stock').click(addSelection);
-  $('.selection').append(clone);
-}
 jQuery(function($){
-  addSelection();
+  $('.template').click(function(){
+    $('.searching-box').show();
+  });
 })
